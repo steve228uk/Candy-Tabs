9b8e5bc9baf3911b51e24c8e174a98da5700531c
Stephen Radford <steve228uk@gmail.com>
2012-10-04 12:23:50 +0100

    Completely re-written

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -1,22 +1,37 @@
-/*
-*	TABS PLUGIN BY STEPHEN RADFORD
-*	COPYRIGHT 2011 STEPHEN RADFORD
-*	ALL RIGHTS RESERVED
-*/
-
-$(document).ready(function() {
-	$('.box-link').click(function tabbedbox() {
-		
-		var box = $(this).attr('href');
-		var activetab = $('#links').children();
-			
-		$(activetab).removeClass('active-tab');
-		$(this).addClass('active-tab');
-		$('.active').removeClass('active');
-		$(box).addClass('active');
-		
-		return false
-		
-	});
+(function($) {
 	
-});
+	$.fn.tabs = function(options) {
+			
+		var defaults = {
+			'active' : 'active',
+			'activetab' : 'active-tab'
+		};
+
+		var options = $.extend({}, defaults, options);
+		
+		return this.each(function() {
+
+			var obj = $(this),
+				tabs = obj.children('ul').children('li').children('a');
+
+			obj.children('div').hide();
+
+			tabs.click(function(e){
+
+				e.preventDefault();
+
+				var box = $(this).attr('href');
+
+				obj.children('ul').children('li').children('.'+options.activetab).removeClass(options.activetab);
+				$(this).addClass(options.activetab);
+
+				$('.'+options.active).removeClass(options.active);
+				$(box).addClass(options.active);
+
+			});
+
+		});
+		
+	};
+	
+})(jQuery);
